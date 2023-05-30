08f9aa100617d44cd2036c9bfef56cbc0fd9a55c
Adithya Ajay <adithyanajay@gmail.com>
2023-05-30 17:08:03 +0530

    First searchbar completed, need to add flags and store the data to the local storage

---
+++ src/api.js
@@ -0,0 +1,15 @@
+//import axios from "axios";
+
+export let search = ""
+
+export const GEO_LOC_OPT = {
+  method: 'GET',
+  url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/places',
+  headers: {
+    'X-RapidAPI-Key': '1810c56ce6msh782299c71299b6dp1bf6b0jsn7370bd82d5b0',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/places';
+
